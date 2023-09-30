53b7845026cd622ab5e1d3f5cd11752c98e6c4a1
karlitoyo <karltimmins@gmail.com>
2023-09-30 16:43:37 +0100

    added further styling to home page to include wrapper for contract time periods

--- components/homepage/layout/mainSection.tsx
+++ components/homepage/layout/mainSection.tsx
@@ -3,67 +3,82 @@ import * as React from "react";
 const HomeMain = () => (
   <section>
     <div className="box">
-      <div className="stats bg-primary text-primary-content">
-        <div className="stat">
-          <div className="stat-title">Account balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm btn-success">Add funds</button>
+      <div className="indicator">
+        <span className="indicator-item indicator-center badge badge-primary">
+          6 Month Vault
+        </span>
+        <div className="stats bg-primary text-primary-content">
+          <div className="stat">
+            <div className="stat-title">Account balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm btn-success">Add funds</button>
+            </div>
           </div>
-        </div>
 
-        <div className="stat">
-          <div className="stat-title">Current balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm">Withdrawal</button>
-            <button
-              className="btn btn-sm"
-              onClick={async () => {
-                const accounts = await window.ethereum.request({
-                  method: "eth_requestAccounts",
-                });
-                console.log(accounts);
-              }}
-            >
-              deposit
-            </button>
+          <div className="stat">
+            <div className="stat-title">Current balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm">Withdrawal</button>
+              <button
+                className="btn btn-sm"
+                onClick={async () => {
+                  const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                  });
+                  console.log(accounts);
+                }}
+              >
+                deposit
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="stats bg-primary text-primary-content">
-        <div className="stat">
-          <div className="stat-title">Account balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm btn-success">Add funds</button>
+      <div className="indicator">
+        <span className="indicator-item indicator-center badge badge-primary">
+          12 Month Vault
+        </span>
+        <div className="stats bg-primary text-primary-content">
+          <div className="stat">
+            <div className="stat-title">Account balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm btn-success">Add funds</button>
+            </div>
           </div>
-        </div>
 
-        <div className="stat">
-          <div className="stat-title">Current balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm">Withdrawal</button>
-            <button className="btn btn-sm">deposit</button>
+          <div className="stat">
+            <div className="stat-title">Current balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm">Withdrawal</button>
+              <button className="btn btn-sm">deposit</button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="stats bg-primary text-primary-content">
-        <div className="stat">
-          <div className="stat-title">Account balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm btn-success">Add funds</button>
+      <div className="indicator">
+        <span className="indicator-item indicator-center badge badge-primary">
+          24 Month Vault
+        </span>
+        <div className="stats bg-primary text-primary-content">
+          <div className="stat">
+            <div className="stat-title">Account balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm btn-success">Add funds</button>
+            </div>
           </div>
-        </div>
 
-        <div className="stat">
-          <div className="stat-title">Current balance</div>
-          <div className="stat-value">$89,400</div>
-          <div className="stat-actions">
-            <button className="btn btn-sm">Withdrawal</button>
-            <button className="btn btn-sm">deposit</button>
+          <div className="stat">
+            <div className="stat-title">Current balance</div>
+            <div className="stat-value">$89,400</div>
+            <div className="stat-actions">
+              <button className="btn btn-sm">Withdrawal</button>
+              <button className="btn btn-sm">deposit</button>
+            </div>
           </div>
         </div>
       </div>
