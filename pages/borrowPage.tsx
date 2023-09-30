30efacf5003bbe50aefad14eedcef5632a685294
karlitoyo <karltimmins@gmail.com>
2023-09-30 16:31:31 +0100

    adding styling to borrow page

--- pages/borrowPage.tsx
+++ pages/borrowPage.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import BorrowHeader from "../components/borrowpage/borrowPageHeader";
+import BorrowMain from "../components/borrowpage/layout/mainSection";
 
 const BorrowPage = () => (
   <Layout title="Borrow Page">
     <BorrowHeader />
+    <BorrowMain />
   </Layout>
 );
 
